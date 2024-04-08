dc88ac3f231fc575cfaa262fd0d6ca50e01ba9c6
Pablo Lasarte <pl8n4@umsystem.edu>
2024-04-08 18:21:59 -0500

    Added and called API setup routes for Standings view

--- app.py
+++ app.py
@@ -10,15 +10,20 @@ def create_app():
     
     db.init_app(app)
     
-    # Import routes here to avoid circular imports
+    # Routes import
     from routes.player_routes import setup_player_routes
     from routes.team_routes import setup_team_routes
     from routes.match_routes import setup_match_routes
+    from routes.contract_routes import setup_contract_routes
+    from routes.scorer_assister_routes import setup_assister_routes, setup_scorer_routes
     
     # Initialize route setups
     setup_player_routes(app)
     setup_team_routes(app)
     setup_match_routes(app)
+    setup_contract_routes(app)
+    setup_scorer_routes(app)
+    setup_assister_routes(app)
     
     return app
 
